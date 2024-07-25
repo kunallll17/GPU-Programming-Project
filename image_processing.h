574878b76ef1218b4eca4a5e41a11759ba6ed379
kunallll17 <120559248+kunallll17@users.noreply.github.com>
2024-07-25 20:29:26 +0530

    Create image_processing.h

---
+++ image_processing.h
@@ -0,0 +1,6 @@
+#ifndef IMAGE_PROCESSING_H
+#define IMAGE_PROCESSING_H
+
+void applyGaussianBlur(const unsigned char* input, unsigned char* output, int width, int height, int channels);
+
+#endif // IMAGE_PROCESSING_H
